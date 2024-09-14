b20f1e434b181e3da0ad8c92fe8be65ee9a12915
ashishui <ashishcop@gmail.com>
2024-09-14 21:15:49 +0530

    local db mogo post request created

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 mongoose.connect('mongodb://127.0.0.1:27017/local',{
   
 }).then(()=>{
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const Product = require('./../models/product.model');
+
 
 const router = express.Router();
+
+
 
 router.get('/',(req,res,next)=>{
     res.send(`OK!!!!!!!!!!`);
@@ -9,6 +13,10 @@ router.get('/',(req,res,next)=>{
 
 
 router.post('/',(req,res,next)=>{
+    const product = new Product({name: req.body.name, price:req.body.price});
+    product.save().then(result=>{console.log(result); res.send(result);}).catch(err=>console.log(err.message));
+    
+    console.log(req.body);
     res.send(`product created`);
     
 });
